73deb07d0b64cb7cd4279a9a6721213d8381b804
Ruziev-Gofur <gafur_ru@bk.ru>
2022-08-24 00:57:50 +0500

    tablitsalar bir biriga ulandi

--- database/migrations/2022_08_04_173225_crate_products_table.php
+++ database/migrations/2022_08_04_173225_crate_products_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table){
            $table->id();
            $table->string('name');
-           $table->string('type');
+           $table->foreignId('type')->constrained();
            $table->string('weight');
            $table->integer('price');
            $table->timestamps();
